e96ac54e894f43c6bdac4d7247ab8f435937644f
Alexandru Toma <alexandrutoma@android-77756700812729cc.localdomain>
2017-11-12 01:15:53 +0000

    Solved gili problem

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from neural_net import Net
 import sys
 import math
+
+save_file = "./net"
+data_file = "./data.in"
 
 def train(net, data):
     net.random_weights()
     net.start_session()
     net.print_net()
     net.train(data)
-    net.save_to_file("part1_net")
+    net.save_to_file(save_file)
     net.print_net()
 
 # Net(hidden_layers, nr_of_neurons_per_layer, inputs, outputs, batch)
-n = Net(1, 10, 2, 1, 10)
+inputs = 3
+outputs = 2
+n = Net(1, 10, inputs, outputs, 10)
 
 if (sys.argv[1] == "train"):
-	data = []
-	for i in range(0, 10000):
-		data.append([1, 62, 63])
-		data.append([2, 15, 17])
-		data.append([9, 10, 19])
-		data.append([11, 21, 32])
-	train(n, data)
+    data = []
+    with open(data_file) as f:
+        for line in f.readlines():
+            line_data = line.strip().split(" ")
+            data.append(list(map(lambda x: float(x), line_data)))
+    train(n, data)
 elif (sys.argv[1] == "solve"):
-    data = [[float(sys.argv[2]), float(sys.argv[3])]]
-    n.weights_from_file("part1_net")
+    data = [[float(sys.argv[i]) for i in range(2, 2+outputs+1)]]
+    n.weights_from_file(save_file)
     res = n.propagate(data)
     print(res)
